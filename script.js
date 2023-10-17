4d76b83fee033158714581a619844781b8ce7c9a
Javed Ahmed <dexjaved@gmail.com>
2023-10-17 21:06:19 +0530

    recording api added

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 let video = document.querySelector("video");
+let recordBtnContainer = document.querySelector(".record-btn-container"); // for recording
+let captureBtnContainer = document.querySelector(".capture-btn-container");
+let recordBtn = document.querySelector(".record-btn"); // for animation
+let captureBtn = document.querySelector(".capture-btn")
+
+let recordFlag = false;
+
+
+let recorder;
 
 let constrains = {
     video: true,
     audio: true
 }
 console.log("calleds")
-// navigator.mediaDevices.getUserMedia(constrains) // return promise
-//     .then((stream) => {
-//         video.srcObject = stream
-//     })
+navigator.mediaDevices.getUserMedia(constrains) // return promise
+    .then((stream) => {
+        video.srcObject = stream
+
+        recorder = new MediaRecorder(stream)
+    })
+
+recordBtnContainer.addEventListener("click", (event) => {
+    if(!record) return;
+
+    recordFlag = !recordFlag;
+
+    if(recordFlag){ // start recording
+        recorder.start();
+        recordBtn.classList.add("scale-record")
+
+    }else{ // stop
+        recorder.stop();
+        recordBtn.classList.remove("scale-record")
+
+    }
+
+
+})    
+captureBtnContainer.addEventListener("click",(event) => {
+
+})
