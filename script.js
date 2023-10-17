b332b05ddacee997095a7b3bef38679849db1162
Javed Ahmed <dexjaved@gmail.com>
2023-10-17 19:51:17 +0530

    scream from webcam added to video element

---
+++ script.js
@@ -0,0 +1,11 @@
+let video = document.querySelector("video");
+
+let constrains = {
+    video: true,
+    audio: true
+}
+console.log("calleds")
+navigator.mediaDevices.getUserMedia(constrains) // return promise
+    .then((stream) => {
+        video.srcObject = stream
+    })
